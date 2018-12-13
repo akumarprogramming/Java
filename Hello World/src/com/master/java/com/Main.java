bd61184b30b9d316daa76103890d6280aa58b755
axk3818 <amit_kumar@homedepot.com>
2018-12-12 22:21:32 -0500

    Initial Commit

---
+++ Hello World/src/com/master/java/com/Main.java
@@ -0,0 +1,18 @@
+package com.master.java.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Shows that white spaces dont make a diff sentences are delimited by semicolon
+        System.out.println("Hello World 1");
+        System.out.println(       "Hello World 2"    );
+        System.out.println(
+                "Hello World 3"
+        );
+        /* Show that \n is new line differnciator*/
+        System.out.println("Hello World \n 3");
+
+        /** Sample Java Doc*/
+    }
+}
